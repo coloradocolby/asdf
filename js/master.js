3eb9d4e0ddf2bcb14cd08630285618a7995bb1c5
ColoradoColby <coloradocolby@gmail.com>
2017-02-07 12:17:38 -0700

    added forms and typed.js

--- js/master.js
+++ js/master.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-  
+$(function(){
   var hasBegun = 0;
 
   function isScrolledIntoView(elem)
@@ -17,7 +16,7 @@ $(document).ready(function() {
       {
         hasBegun = 1;
           $(".typedout").typed({
-            strings: ["First sentence.", "Second sentence."],
+            strings: ["We are <strong>creatives</strong>", "We are <strong>innovators</strong>.", "We bleed <span class='blue'><strong>blue</strong></span>"],
             // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
             stringsElement: null,
             // typing speed
@@ -54,6 +53,7 @@ $(document).ready(function() {
             backspace: function(curString, curStrPos) {
               setTimeout(function() {
 
+                console.log('in backspace');
                 // check string array position
                 // on the first string, only delete one word
                 // the stopNum actually represents the amount of chars to
@@ -99,67 +99,3 @@ $(document).ready(function() {
     transition: function(url){ window.location.href = url; }
   });
 });
-
-
-
-
-
-// $(function() {
-//   $(".typedout").typed({
-//     strings: ["First sentence.", "Second sentence."],
-//     // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-//     stringsElement: null,
-//     // typing speed
-//     typeSpeed: 0,
-//     // time before typing starts
-//     startDelay: 0,
-//     // backspacing speed
-//     backSpeed: 0,
-//     // shuffle the strings
-//     shuffle: false,
-//     // time before backspacing
-//     backDelay: 500,
-//     // loop
-//     loop: false,
-//     // false = infinite
-//     loopCount: false,
-//     // show cursor
-//     showCursor: true,
-//     // character for cursor
-//     cursorChar: "",
-//     // attribute to type (null == text)
-//     attr: null,
-//     // either html or text
-//     contentType: 'html',
-//     // call when done callback function
-//     callback: function() {},
-//     // starting callback function before each string
-//     preStringTyped: function() {},
-//     //callback for every typed string
-//     onStringTyped: function() {},
-//     // callback for reset
-//     resetCallback: function() {},
-//
-//     backspace: function(curString, curStrPos) {
-//       setTimeout(function() {
-//
-//         // check string array position
-//         // on the first string, only delete one word
-//         // the stopNum actually represents the amount of chars to
-//         // keep in the current string. In my case it's 3.
-//         if (self.arrayPos == 3) {
-//           self.stopNum = 5;
-//         }
-//         //every other time, delete the whole typed string
-//         else {
-//           self.stopNum = 0;
-//         }
-//
-//
-//
-//       })
-//     }
-//
-//
-//   });
-// });
