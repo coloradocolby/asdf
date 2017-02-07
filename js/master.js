77f51bf6c8a966c2d018d5ff715268d18f0b199e
coloradocolby <coloradocolby@gmail.com>
2017-02-06 22:30:26 -0700

    more progress

---
+++ js/master.js
@@ -0,0 +1,59 @@
+// $(function() {
+//   $(".typedout").typed({
+//     strings: ["First sentence.", "Second sentence."],
+//     // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+//     stringsElement: null,
+//     // typing speed
+//     typeSpeed: 0,
+//     // time before typing starts
+//     startDelay: 0,
+//     // backspacing speed
+//     backSpeed: 0,
+//     // shuffle the strings
+//     shuffle: false,
+//     // time before backspacing
+//     backDelay: 500,
+//     // loop
+//     loop: false,
+//     // false = infinite
+//     loopCount: false,
+//     // show cursor
+//     showCursor: true,
+//     // character for cursor
+//     cursorChar: "",
+//     // attribute to type (null == text)
+//     attr: null,
+//     // either html or text
+//     contentType: 'html',
+//     // call when done callback function
+//     callback: function() {},
+//     // starting callback function before each string
+//     preStringTyped: function() {},
+//     //callback for every typed string
+//     onStringTyped: function() {},
+//     // callback for reset
+//     resetCallback: function() {},
+//
+//     backspace: function(curString, curStrPos) {
+//       setTimeout(function() {
+//
+//         // check string array position
+//         // on the first string, only delete one word
+//         // the stopNum actually represents the amount of chars to
+//         // keep in the current string. In my case it's 3.
+//         if (self.arrayPos == 3) {
+//           self.stopNum = 5;
+//         }
+//         //every other time, delete the whole typed string
+//         else {
+//           self.stopNum = 0;
+//         }
+//
+//
+//
+//       })
+//     }
+//
+//
+//   });
+// });
